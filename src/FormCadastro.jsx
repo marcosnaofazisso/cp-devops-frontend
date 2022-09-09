21ac28d3e598d72761ca41d9b7c933c73796d999
Marcos Vinicius Ferreira <marcosferreiraf22@gmail.com>
2022-09-09 17:23:56 -0300

    clean code

--- src/FormCadastro.jsx
+++ src/FormCadastro.jsx
@@ -29,55 +29,17 @@ function FormCadastro() {
 
     const url = "http://localhost:8080/api/cliente"
 
-    function changeNome(event) {
-        setCliente(cliente => ({ ...cliente, nome: event.target.value }))
-    }
-    function changeEmail(event) {
-        setCliente(cliente => ({ ...cliente, email: event.target.value }))
-    }
-    function changeCpf(event) {
-        setCliente(cliente => ({ ...cliente, cpf: event.target.value }))
-    }
-    function changeLogradouro(event) {
-        setEndereco(endereco => ({ ...endereco, logradouro: event.target.value }))
-    }
-    function changeNumeroLogradouro(event) {
-        setEndereco(endereco => ({ ...endereco, numeroEndereco: event.target.value }))
-    }
-    function changeBairro(event) {
-        setEndereco(endereco => ({ ...endereco, bairro: event.target.value }))
-    }
-    function changeCidade(event) {
-        setEndereco(endereco => ({ ...endereco, cidade: event.target.value }))
-    }
-    function changeEstado(event) {
-        setEndereco(endereco => ({ ...endereco, estado: event.target.value }))
-    }
-    function changeCep(event) {
-        setEndereco(endereco => ({ ...endereco, cep: event.target.value }))
-    }
 
 
+    function handleChangeCliente(event) {
+        setCliente(cliente => ({ ...cliente, [event.target.name]: event.target.value }))
+    }
+    function handleChangeEndereco(event) {
+        setEndereco(endereco => ({ ...endereco, [event.target.name]: event.target.value }))
+    }
 
-
-
-    function changeLogradouro2(event) {
-        setEndereco2(endereco2 => ({ ...endereco2, logradouro: event.target.value }))
-    }
-    function changeNumeroLogradouro2(event) {
-        setEndereco(endereco2 => ({ ...endereco2, numeroEndereco: event.target.value }))
-    }
-    function changeBairro2(event) {
-        setEndereco2(endereco2 => ({ ...endereco2, bairro: event.target.value }))
-    }
-    function changeCidade2(event) {
-        setEndereco2(endereco2 => ({ ...endereco2, cidade: event.target.value }))
-    }
-    function changeEstado2(event) {
-        setEndereco2(endereco2 => ({ ...endereco2, estado: event.target.value }))
-    }
-    function changeCep2(event) {
-        setEndereco2(endereco2 => ({ ...endereco2, cep: event.target.value }))
+    function handleChangeEndereco2(event) {
+        setEndereco2(endereco2 => ({ ...endereco2, [event.target.name]: event.target.value }))
     }
 
     const handleSubmit = (e) => {
@@ -112,6 +74,8 @@ function FormCadastro() {
             .catch((err) => { console.log(err.message) })
     }
 
+
+
     return (
         <>
             <div>
@@ -123,39 +87,40 @@ function FormCadastro() {
                         <h3>Email:{cliente.email}</h3>
                         <h3>CPF: {cliente.cpf}</h3> */}
                         <label>Nome</label>
-                        <input type="text" value={cliente.nome} onChange={changeNome} required />
+                        <input type="text" name="nome" value={cliente.nome} onChange={handleChangeCliente} required />
                         <br />
                         <label>Email</label>
-                        <input type="text" value={cliente.email} onChange={changeEmail} required />
+                        <input type="text" name="email" value={cliente.email} onChange={handleChangeCliente} required />
                         <br />
                         <label>CPF</label>
-                        <input type="text" value={cliente.cpf} onChange={changeCpf} required />
+                        <input type="text" name="cpf" value={cliente.cpf} onChange={handleChangeCliente} required />
                         <br />
                     </div>
                     <div>
                         <h2>Dados de Endereço</h2>
-                        {/* <h3>Logradouro: {endereco.logradouro}</h3>
+                        <h3>Logradouro: {endereco.logradouro}</h3>
                         <h3>Número: {endereco.numeroEndereco}</h3>
-                        <h3>Cidade: {endereco.cidade}</h3>
                         <h3>Bairro:{endereco.bairro}</h3>
-                        <h3>CEP: {endereco.cep}</h3>  */}
+                        <h3>Cidade: {endereco.cidade}</h3>
+                        <h3>Estado: {endereco.estado}</h3>
+                        <h3>CEP: {endereco.cep}</h3>
                         <label>Logradouro</label>
-                        <input type="text" value={endereco.logradouro} onChange={changeLogradouro} />
+                        <input type="text" name="logradouro" value={endereco.logradouro} onChange={handleChangeEndereco} />
                         <br />
                         <label>Número</label>
-                        <input type="text" value={endereco.numeroEndereco} onChange={changeNumeroLogradouro} />
+                        <input type="text" name="numeroEndereco" value={endereco.numeroEndereco} onChange={handleChangeEndereco} />
                         <br />
                         <label>Bairro</label>
-                        <input type="text" value={endereco.bairro} onChange={changeBairro} />
+                        <input type="text" name="bairro" value={endereco.bairro} onChange={handleChangeEndereco} />
                         <br />
                         <label>Cidade</label>
-                        <input type="text" value={endereco.cidade} onChange={changeCidade} />
+                        <input type="text" name="cidade" value={endereco.cidade} onChange={handleChangeEndereco} />
                         <br />
                         <label>Estado</label>
-                        <input type="text" value={endereco.estado} onChange={changeEstado} />
+                        <input type="text" name="estado" value={endereco.estado} onChange={handleChangeEndereco} />
                         <br />
                         <label>CEP</label>
-                        <input type="text" value={endereco.cep} onChange={changeCep} />
+                        <input type="text" name="cep" value={endereco.cep} onChange={handleChangeEndereco} />
                     </div>
                     <br />
                     <br />
@@ -168,22 +133,22 @@ function FormCadastro() {
                     <div>
                         <h2>Endereço Adicional</h2>
                         <label>Logradouro</label>
-                        <input type="text" value={endereco2.logradouro} onChange={changeLogradouro2} />
+                        <input type="text" name="logradouro" value={endereco2.logradouro} onChange={handleChangeEndereco2} />
                         <br />
                         <label>Número</label>
-                        <input type="text" value={endereco2.numeroEndereco} onChange={changeNumeroLogradouro2} />
+                        <input type="text" name="numeroEndereco" value={endereco2.numeroEndereco} onChange={handleChangeEndereco2} />
                         <br />
                         <label>Bairro</label>
-                        <input type="text" value={endereco2.bairro} onChange={changeBairro2} />
+                        <input type="text" name="bairro" value={endereco2.bairro} onChange={handleChangeEndereco2} />
                         <br />
                         <label>Cidade</label>
-                        <input type="text" value={endereco2.cidade} onChange={changeCidade2} />
+                        <input type="text" name="cidade" value={endereco2.cidade} onChange={handleChangeEndereco2} />
                         <br />
                         <label>Estado</label>
-                        <input type="text" value={endereco2.estado} onChange={changeEstado2} />
+                        <input type="text" name="estado" value={endereco2.estado} onChange={handleChangeEndereco2} />
                         <br />
                         <label>CEP</label>
-                        <input type="text" value={endereco2.cep} onChange={changeCep2} />
+                        <input type="text" name="cep" value={endereco2.cep} onChange={handleChangeEndereco2} />
                         <br />
                         <br />
                         <button onClick={() => setNovoEndereco(!novoEndereco)}>Cancelar</button>
